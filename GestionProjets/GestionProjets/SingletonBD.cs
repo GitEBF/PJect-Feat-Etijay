d911fbde21b2f11c22e0ffccf7ff3daf074e7e6f
tijaycegep <143433393+tijaycegep@users.noreply.github.com>
2023-11-14 14:41:36 -0500

    Installation MySql

---
+++ GestionProjets/GestionProjets/SingletonBD.cs
@@ -0,0 +1,49 @@
+﻿using Microsoft.WindowsAppSDK.Runtime.Packages;
+using MySql.Data.MySqlClient;
+using MySqlX.XDevAPI;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GestionProjets
+{
+    internal class SingletonBD
+    {
+        MySqlConnection con = new MySqlConnection("Server=cours.cegep3r.info;Database=2248734-thibeault-jérémy;Uid=2248734;Pwd=2248734;");
+        static SingletonBD instance = null;
+
+        public SingletonBD()
+        {
+        }
+
+        public static SingletonBD getInstance()
+        {
+            if (instance == null)
+                instance = new SingletonBD();
+
+            return instance;
+        }
+
+        public void edit(string CommandText)
+        {
+            try
+            {
+                MySqlCommand commande = new MySqlCommand();
+                commande.Connection = con;
+                commande.CommandText = CommandText;
+                con.Open();
+                commande.ExecuteNonQuery();
+                con.Close();
+            }
+            catch { }
+
+        }
+
+
+
+    }
+}
